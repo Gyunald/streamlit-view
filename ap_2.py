28a3710fb82d072cc1f86ef119b59eb6389efa6b
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-10-13 22:37:42 +0900

    Add files via upload

--- ap_2.py
+++ ap_2.py
@@ -1,83 +1,110 @@
-import random
-import streamlit as st
+import numpy as np
+import altair as alt
 import pandas as pd
+import streamlit as st
+from datetime import datetime, time
+import time
 
-# text
+# # 1 헤더 텍스트를 만듭니다.
+# st.header('st.write') 
 
-st.title("IMI CCI") # 웹 대시보드
-st.header("IMI CCI") # 제목같은 큰 글씨
-st.subheader("IMI CCI") # 제목보다는 작은 글씨
-st.text("IMI CCi") # 작은 글씨
+# # 2 ** 기울기, :: 이모티콘
+# st.write('Hello, *World!* :sunglasses:') 
 
-st.success("Success는 녹색") # 녹색 영역표시
-st.warning("Warning은 노란색") # 노란색 영역표시
-st.info("Info는 파란색") # 파란색 영역표시
-st.error("Error는 빨간색") # 빨간 영역표시
+# # 3 숫자와 같은 다른 데이터 형식을 표시하는 데에도 사용할 수 있습니다.
+# st.write(1234)
 
-# st.help(sum) # 함수 사용법 ex) sum 사용법
+# # 4 DataFrames는 다음과 같이 표시될 수도 있습니다.
+# df = pd.DataFrame({ 
+#     'fitst column' : [1,2,3,4],
+#     'second column' : [10,20,30,40]
+# })
+# st.write(df)
 
-# dataframe
+# # 5 여러 인수를 전달할 수 있습니다.
+# st.write('Below is a DateFrame:', df, 'Above is a dataframe.')
 
-# df = pd.read_csv("ec.csv",encoding="cp949")
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40],
-  'third cloumn' : ["a","b","c","d"]
-})
+# # 6 변수에 전달하여 플롯도 표시할 수 있습니다.
+# df2 = pd.DataFrame(
+#     np.random.randn(200,3),
+#     columns=['a', 'b', 'c'])
+# c = alt.Chart(df2).mark_circle().encode(
+#     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+# st.write(c)
 
-st.dataframe(df)
+# st.caption('This is a string that explains something above.')
 
-a = df['second column'].unique() # unique() : 고유값
-st.text("second column은" + str(a) + "대 이다")
+# Slider
+# st.header('st.slider')
 
-st.write('head',df.head()) # head() 앞에서 5개
-st.write('tail',df.tail()) # tail() 뒤에서 5개
+# st.subheader("Slider")
+# age = st.slider('How old are you?',1,100,20,3,) # 내용,최소,최대,기본값,스텝
+# st.write("I'm ", age, 'years old')
 
+# st.subheader('Range slider')
+# value = st.slider(
+#     'Select a range of values',0.0, 100.0, (25.0, 75.0)) # 튜플로 기본값 설정
+# st.write('Values: ', value)
 
-# 버튼 만들기
+# st.subheader('Range time slider')
+# appointment = st.slider(
+#     "Schedule your appointment:",
+#     value=(time(8,30),time(12,00))
+# )
+# st.write("You're scheduled for: ", appointment[0],appointment[1])
 
-st.title("Make Button")
-if st.button("click me!"):
-    st.subheader('second cloumn.unique')
-    st.dataframe(df['second column'].unique())
-else :
-    st.write("Hi")
+# start_time = st.slider(
+#     "When do you start?",
+#     value = datetime(2022,1,1,9,30),
+#     format="MM/DD/YY - hh:mm"
+# )
+# st.write("Start time:", start_time)
 
-st.title("Make Button")
-if st.button("click me"):    
-    st.subheader('third cloumn.unique')
-    st.dataframe(df['third cloumn'].unique())
-else :
-    st.write("Hi")
+# st.line_chart
 
-# 라디오(radio) 버튼 만들기
-# 클릭했을때, 나타나는 문자열을 리턴해줌
+# st.header('Line chart')
+# chart_data = pd.DataFrame(
+#     np.random.randn(20,3),
+#     columns=['a','b','c'],
+# )
+# st.line_chart(chart_data)
 
-status = st.radio("choice", pd.Series(["a","b","c"]))
-st.header(status)
+# st.selectbox
 
-a = st.radio("정렬방식", pd.Series(["오름차순","내림차순"]))
-# a = st.sidebar.radio("정렬방식", ("오름차순","내림차순"))
-# a = st.radio("정렬방식", ("오름차순","내림차순"))
-b = ("오름차순","내림차순")
-if a == b[0]:
-# if a == "오름차순" :
-    st.dataframe(df.sort_values("second column"))
-else :
-    st.dataframe(df.sort_values("second column",ascending=False))
+# st.header('st.selectbox')
+# option = st.selectbox(
+#     'What is your favorite color?',
+#     ('Blue','Red','Green'),
+# )
+# st.write('your favorite color is ', option)
 
-# 체크박스 (체크하면 True)
+# st.header('st.multiselect')
+# options = st.multiselect(
+#     'What is your favorite colors?',
+#     ['Blue','Red','Green','Yellow'],
+#     ['Yellow','Red']
+# )
+# st.write('You selected', options)
 
-if st.checkbox("체크할때만 나옴"):
-    st.write("Hi")
-    st.dataframe(df.head())
+# st.checkbox
 
-# 셀렉트박스 (여러개 중에서 1개만 고르기)
-st.title('IMI CCI')
-option = st.selectbox(
-    '당신의 직책을 선택해주세요.',
-    pd.Series(['CEO', 'AI Engineer', 'Intern', 'Product Manager']))
-# option = st.sidebar.selectbox(
-#     '당신의 직책을 선택해주세요.',
-#     pd.Series(['CEO', 'AI Engineer', 'Intern', 'Product Manager']))
-f'You selected: {option}'
+# st.header('st.checkbox')
+# st.write('What would you like to order?')
+# icecream = st.checkbox('Icecream')
+# coffee = st.checkbox('Coffee')
+# cola = st.checkbox("Cola")
+
+# if icecream:
+#     st.write("Great! here's some more :icecream:")
+
+# if coffee:
+#     st.write("Okay, here's some more :coffee:")
+    
+# if cola:
+#     st.write("Here you go")
+
+import streamlit as st
+
+st.title('st.secrets')
+
+st.write(st.secrets['name'])

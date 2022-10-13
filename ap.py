84a3126110fb8772ed893f803dbb5911f2725e2c
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-10-13 21:05:14 +0900

    Add files via upload

--- ap.py
+++ ap.py
@@ -4,10 +4,12 @@ st.write("id", st.secrets["gyunald"])
 
 import os
 st.write("hi",os.environ["gyunald"] == st.secrets["gyunald"])
-
+st.write(st.secrets["rbskfem"])
 st.header("st.button") 
 
 if st.button('Say hello'):
     st.write('Why hello there')
 else:
     st.write('Goodbye')
+    
+

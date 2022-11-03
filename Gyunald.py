ddcadd5d5c34207db5b4c1ded278591da7f7dac8
Gyunald <97107396+Gyunald@users.noreply.github.com>
2022-11-03 12:41:08 +0900

    Add files via upload

--- Gyunald.py
+++ Gyunald.py
@@ -1,4 +1,10 @@
 import streamlit as st
 import random
+
+st.secrets.username
+st.secrets.password
+
 st.title('Lucky 6')
 st.write(*sorted(random.sample(range(1,46),6)))
+
+st.write(st.secrets.username)
